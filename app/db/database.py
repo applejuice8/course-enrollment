c9edf8f2775d9d801636c87c8ce6199d9e6e12c0
Colin <colinleonghongseng@gmail.com>
2025-11-12 17:56:20 +0800

    added dependencies

---
+++ app/db/database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
+from app.core.config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
