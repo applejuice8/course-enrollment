be0425895e19096598310a76a72dd9d00ac9edd9
Colin <colinleonghongseng@gmail.com>
2025-11-12 18:57:24 +0800

    added hash function in security.py

---
+++ app/core/security.py
@@ -0,0 +1,9 @@
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def hash_pw(pw: str) -> str:
+    return pwd_context.hash(pw)
+
+def verify_pw(plain_pw: str, hashed_pw: str) -> bool:
+    return pwd_context.verify(plain_pw, hashed_pw)
