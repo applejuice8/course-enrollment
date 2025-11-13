3d69343154093c555cf9b6a0074d64790e666f11
Colin <colinleonghongseng@gmail.com>
2025-11-13 14:08:15 +0800

    added newline to print message in fast_commit

--- fast_commit.bash
+++ fast_commit.bash
@@ -5,11 +5,14 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
+echo
 echo "Executing 'git add .'"
 git add .
 
+echo
 echo "Executing 'git commit -m \"$1\"'"
 git commit -m "$1"
 
+echo
 echo "Executing 'git push'"
 git push
