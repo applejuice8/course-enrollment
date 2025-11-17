d952c2dfecba92d4a5db5988c0281c132060b4ca
Colin <colinleonghongseng@gmail.com>
2025-11-17 21:42:43 +0800

    added validation to CourseCreate pydantic model

--- app/schemas/course.py
+++ app/schemas/course.py
@@ -1,9 +1,26 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 class CourseCreate(BaseModel):
-    code: str
-    name: str
-    credit_hours: int
+    code: str = Field(
+        ...,
+        title='Course Code',
+        pattern='^[A-Za-z]{3}[0-9]{4}$',
+        example='FEL1234'
+    )
+    name: str = Field(
+        ...,
+        title='Course Name',
+        min_length=10,
+        max_length=50,
+        example='Functional Programming Principles'
+    )
+    credit_hours: int = Field(
+        title='Credit Hours',
+        description='Number of credit hours per week',
+        gt=0,
+        lt=10,
+        example=4
+    )
 
 class CourseGet(BaseModel):
     id: int
