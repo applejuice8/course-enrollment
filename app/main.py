ff77c7bd90683d186d65394e29d767be87563187
Colin <colinleonghongseng@gmail.com>
2025-11-19 18:24:13 +0800

    removed if __name__ equals __main__ in main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,3 @@ app = FastAPI()
 app.include_router(course.router)
 app.include_router(enrollment.router)
 app.include_router(student.router)
-
-if __name__ == '__main__':
-    import uvicorn
-    uvicorn.run('app.main:app', reload=True)
