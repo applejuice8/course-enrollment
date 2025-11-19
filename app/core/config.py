456cdee6f9c43ea82f8648ae738fd703d7df517b
Colin <colinleonghongseng@gmail.com>
2025-11-19 16:03:54 +0800

    changed config.py to use pydantic model

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,9 @@
-import os
-from dotenv import load_dotenv
+from pydantic import BaseSettings
 
-load_dotenv()
+class Settings(BaseSettings):
+    database_url: str
 
-DATABASE_URL = os.getenv('DATABASE_URL')
+    class Config:
+        env_file = '.env'
+
+settings = Settings()
